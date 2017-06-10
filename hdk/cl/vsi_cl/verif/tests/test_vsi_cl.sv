f9784a140f6340795d71a0fee5c5058e1e4fe381
Sandeep Dutta <sandeep@systemviewinc.com>
2017-06-10 09:56:27 -0700

    adding test case

--- hdk/cl/vsi_cl/verif/tests/test_vsi_cl.sv
+++ hdk/cl/vsi_cl/verif/tests/test_vsi_cl.sv
@@ -14,7 +14,7 @@
 // limitations under the License.
 
 
-module test_hello_world();
+module test_vsi_cl();
 
 import tb_type_defines_pkg::*;
 `include "cl_common_defines.vh" // CL Defines with register addresses
@@ -36,29 +36,6 @@ logic [15:0] vled_value;
       vdip_value = tb.get_virtual_dip_switch();
 
       $display ("value of vdip:%0x", vdip_value);
-
-      $display ("Writing 0xDEAD_BEEF to address 0x%x", `HELLO_WORLD_REG_ADDR);
-      tb.poke(.addr(`HELLO_WORLD_REG_ADDR), .data(32'hDEAD_BEEF), .id(AXI_ID), .size(DataSize::UINT16), .intf(AxiPort::PORT_OCL)); // write register
-
-      tb.peek(.addr(`HELLO_WORLD_REG_ADDR), .data(rdata), .id(AXI_ID), .size(DataSize::UINT16), .intf(AxiPort::PORT_OCL));         // start read & write
-      $display ("Reading 0x%x from address 0x%x", rdata, `HELLO_WORLD_REG_ADDR);
-
-      if (rdata == 32'hEFBE_ADDE) // Check for byte swap in register read
-        $display ("Test PASSED");
-      else
-        $display ("Test FAILED");
-
-      tb.peek_ocl(.addr(`VLED_REG_ADDR), .data(rdata));         // start read
-      $display ("Reading 0x%x from address 0x%x", rdata, `VLED_REG_ADDR);
-
-      if (rdata == 32'h0000_BEEF) // Check for LED register read
-        $display ("Test PASSED");
-      else
-        $display ("Test FAILED");
-
-      vled_value = tb.get_virtual_led();
-
-      $display ("value of vled:%0x", vled_value);
 
       tb.kernel_reset();
 
