be688613f69c4790b3735014864a0dd2c967a765
Sandeep Dutta <sandeep@systemviewinc.com>
2017-06-12 11:12:36 -0700

    created a simulation testbench for vsi_cl

--- hdk/cl/vsi_cl/verif/scripts/top.questa.f
+++ hdk/cl/vsi_cl/verif/scripts/top.questa.f
@@ -19,35 +19,56 @@
 +libext+.sv
 +libext+.svh
 
--y ${CL_ROOT}/../common/design
+-y ${CL_ROOT}/../examples/common/design
 -y ${CL_ROOT}/design
 -y ${SH_LIB_DIR}
 -y ${SH_INF_DIR}
 -y ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/hdl
 -y ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/sim
+-y ${HDK_SHELL_DESIGN_DIR}/sh_ddr/sim
 
-+incdir+${CL_ROOT}/../common/design
++incdir+${CL_ROOT}/../examples/common/design
 +incdir+${CL_ROOT}/verif/sv
 +incdir+${SH_LIB_DIR}
 +incdir+${SH_INF_DIR}
 +incdir+${HDK_COMMON_DIR}/verif/include
-+incdir+${HDK_COMMON_DIR}/verif/include
 +incdir+${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_0/hdl/verilog
++incdir+${CL_ROOT}/design/axi_crossbar_0 
++incdir+${HDK_SHELL_DESIGN_DIR}/sh_ddr/sim
 
-${CL_ROOT}/../common/design/cl_common_defines.vh
-${CL_ROOT}/design/cl_hello_world_defines.vh
+${SH_LIB_DIR}/../ip/cl_axi_interconnect/ip/cl_axi_interconnect_xbar_0/sim/cl_axi_interconnect_xbar_0.v
+${SH_LIB_DIR}/../ip/cl_axi_interconnect/ip/cl_axi_interconnect_s00_regslice_0/sim/cl_axi_interconnect_s00_regslice_0.v
+${SH_LIB_DIR}/../ip/cl_axi_interconnect/ip/cl_axi_interconnect_m00_regslice_0/sim/cl_axi_interconnect_m00_regslice_0.v
+${SH_LIB_DIR}/../ip/cl_axi_interconnect/ip/cl_axi_interconnect_m01_regslice_0/sim/cl_axi_interconnect_m01_regslice_0.v
+${SH_LIB_DIR}/../ip/cl_axi_interconnect/ip/cl_axi_interconnect_m02_regslice_0/sim/cl_axi_interconnect_m02_regslice_0.v
+${SH_LIB_DIR}/../ip/cl_axi_interconnect/ip/cl_axi_interconnect_m03_regslice_0/sim/cl_axi_interconnect_m03_regslice_0.v
+${SH_LIB_DIR}/../ip/cl_axi_interconnect/hdl/cl_axi_interconnect.v
+${SH_LIB_DIR}/../ip/axi_clock_converter_0/sim/axi_clock_converter_0.v
+${SH_LIB_DIR}/../ip/dest_register_slice/sim/dest_register_slice.v
+${SH_LIB_DIR}/../ip/src_register_slice/sim/src_register_slice.v
+${SH_LIB_DIR}/../ip/axi_register_slice/sim/axi_register_slice.v
+${SH_LIB_DIR}/../ip/axi_register_slice_light/sim/axi_register_slice_light.v
+${SH_LIB_DIR}/bram_2rw.sv
+${SH_LIB_DIR}/flop_fifo.sv
+
++define+DISABLE_VJTAG_DEBUG
++define+DISABLE_CHIPSCOPE_DEBUG
+${CL_ROOT}/../examples/common/design/cl_common_defines.vh
+${CL_ROOT}/design/vsi_cl_defines.vh
 ${HDK_SHELL_DIR}/design/ip/ila_vio_counter/sim/ila_vio_counter.v
 ${HDK_SHELL_DIR}/design/ip/ila_0/sim/ila_0.v
 ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/hdl/bd_a493.v
 ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_0/sim/bd_a493_xsdbm_0.v
-${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_0/hdl/xsdbm_v2_0_vl_rfs.v
+${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_0/hdl/xsdbm_v3_0_vl_rfs.v
 ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_0/hdl/ltlib_v1_0_vl_rfs.v
 ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_1/sim/bd_a493_lut_buffer_0.v
-${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_1/hdl/lut_buffer_v1_0_vl_rfs.v
+${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/ip/ip_1/hdl/lut_buffer_v2_0_vl_rfs.v
 ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/bd_0/hdl/bd_a493_wrapper.v
 ${HDK_SHELL_DIR}/design/ip/cl_debug_bridge/sim/cl_debug_bridge.v
 ${HDK_SHELL_DIR}/design/ip/vio_0/sim/vio_0.v
-${CL_ROOT}/design/cl_hello_world.sv
++define+VSI_SIM      
+${CL_ROOT}/design/vsi_cl.sv
+${CL_ROOT}/design/vsi_cl_pkg.sv
 
 -f ${HDK_COMMON_DIR}/verif/tb/filelists/tb.${SIMULATOR}.f
 
